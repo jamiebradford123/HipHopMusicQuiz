53da3eaf8275841ef40ab41864be12f34fe034b4
jamiebradford123 <jamiebradford123@hotmail.com>
2022-05-04 18:05:54 +0000

    add game page with basic html and css structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,4 @@
 // code when DOM has loaded 
 // get buttons and event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "play now"){
-                alert("you clicked play now");
-            }
-        })
-    }
-})
