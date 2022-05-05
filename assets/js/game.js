a72206d37772686bef785063968123d0ee9e6042
jamiebradford123 <jamiebradford123@hotmail.com>
2022-05-05 16:10:10 +0000

    adjust styling on score number and astart js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,5 @@
-console.log("hello there")
+const question = document.querySelector('#question');
+const options = Array.from(document.getSelectorAll('.options-text'));
+const progressText = document.querySelector('progressHeading'); //may not work, we'll see
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
